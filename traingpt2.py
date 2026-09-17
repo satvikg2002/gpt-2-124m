e7781af24d554332218d7e9a74c0b2ce319161ba
Satvik G <satvik.noida@gmail.com>
2026-09-17 16:44:22 -0400

    visualize hf gpt2 then start implementation of personalgpt2 acc to Paper

---
+++ traingpt2.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 6
+    n_head: int = 6
+    n_embd: int = 384
+
+
+class GPT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),   #weights of token embedding
+            wpe = nn.Embedding(config.block_size, config.n_embd),   #weights of positional embed
+            h = nn.ModuleList([Block(config) for _ in range (config.n_layer)]),     #hidden layer list
+            ln_f = nn.LayerNorm(config.n_embd),                     # final layer norm
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)  #classifier
