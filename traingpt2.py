b6f0aefb636c09ae7c551abffe99bb5899503acb
satvikg2002 <satvik.noida@gmail.com>
2026-09-17 22:30:40 -0400

    load HF weights and pass them through GPT Class to get similar results

--- traingpt2.py
+++ traingpt2.py
@@ -103,6 +103,28 @@ class GPT(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)  #classifier
 
 
+    def forward(self, idx):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+
+        # forward the token and posisition embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)      implicitly broadcasted to B Batches to finally be added
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb
+
+        # forward the blocks of the transformer
+        for block in self.transformer.h:
+            x = block(x)
+
+        # forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x)
+
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        return logits
+
+
     @classmethod
     def from_pretrained(cls, model_type):
         """Loads pretrained GPT-2 model weights from huggingface"""
@@ -155,4 +177,54 @@ class GPT(nn.Module):
         return model
 
 
+num_return_sequences = 5
+max_length = 30
+
+
 model = GPT.from_pretrained('gpt2')
+model.eval()
+model.to('cuda')
+
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+
+tokens = torch.tensor(tokens, dtype=torch.long)     # 8 tokens for given string
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)    # (5, 8)
+x = tokens.to('cuda')
+
+# generate for x -> (B, T) 
+# set the seed to 42
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    # forward the model to get the logits
+    with torch.no_grad():
+        logits = model(x) # (B, T, vocab_size)
+
+        # take the logits at the last position
+        logits = logits[:, -1, :] # (B, vocab_size)
+
+        # get the probabilities
+        probs = F.softmax(logits, dim=-1)
+
+        # do top-k sampling of 50 (huggingface pipeline default) 
+        # keep only top 50 probabilities
+        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+
+        # select a token from the top-k probabilities
+        ix = torch.multinomial(topk_probs, 1) # (B, 1)
+
+        # gather the corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+
+        # append to the sequence
+        x = torch.cat((x, xcol), dim=1)
+
+
+# final print
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
