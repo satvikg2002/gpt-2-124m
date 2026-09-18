089d90219386ce103723e5ef96fe6f820a702eb4
satvikg2002 <satvik.noida@gmail.com>
2026-09-18 17:31:19 -0400

    use flash attn to significantly boost performance

--- traingpt2.py
+++ traingpt2.py
@@ -44,10 +44,14 @@ class CausalSelfAttention(nn.Module):
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
         # attention (materializes the large (T,T) matrix for all the queries and keys)
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))         # interact with only past and not future
-        att = F.softmax(att, dim=-1)                                            # normalize output to sum to 1
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)      Weighted some
+        # att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))         # interact with only past and not future
+        # att = F.softmax(att, dim=-1)                                            # normalize output to sum to 1
+        # y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)      Weighted some
+
+        # use flash attention to rewrite and kernel fuse attention into 1 step 
+        # faster even though it does more flops due to memory optimizations
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
         # output projection
         y = self.c_proj(y)
